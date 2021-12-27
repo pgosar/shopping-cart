d3e6348088dc782945917d275ca95f1878360141
pgosar <pgosarsteam@gmail.com>
2021-12-26 21:27:58 -0600

    navigation bar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDom from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
 
-ReactDom.render(<Counters />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
