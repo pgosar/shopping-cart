2104a7dbe20ad65762d19d71b162e89816987203
pgosar <pgosarsteam@gmail.com>
2021-12-26 20:56:30 -0600

    delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,47 +1,48 @@
 import React, { Component } from "react";
 
 class Counter extends React.Component {
-	state = {
-		count: 0,
-	};
+  state = {
+    value: this.props.counter.value,
+  };
 
-	handleIncrement = (product) => {
-		console.log(product);
-		this.setState({ count: this.state.count + 1 });
-	};
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
-	doHandleIncrement = () => {
-		this.handleIncrement({ id: 1 });
-	};
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
 
-	render() {
-		return (
-			<div>
-				<span className={this.getBadgeClasses()}>
-					{this.formatCount()}
-				</span>
-				<button
-					onClick={() => {
-						this.handleIncrement({ id: 1 });
-					}}
-					className="btn btn-secondary btn-sm"
-				>
-					Increment
-				</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
 
-	getBadgeClasses() {
-		let classes = "badge m-2 bg-";
-		classes += this.state.count == 0 ? "warning" : "primary";
-		return classes;
-	}
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.value == 0 ? "warning" : "primary";
+    return classes;
+  }
 
-	formatCount() {
-		const { count } = this.state;
-		return count == 0 ? "Zero" : count;
-	}
+  formatCount() {
+    const { value: count } = this.state;
+    return count == 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
